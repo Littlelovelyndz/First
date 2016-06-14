02908bc28352743c11df9a5ca764909f91027467
Littlelovelyndz <lgunther799@gmail.com>
2016-06-14 12:40:52 -0700

    end of task 13, created search bar

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,40 +1,62 @@
 import Ember from 'ember';
 
 var Photo = Ember.Object.extend({
-		title: '',
-		username: '',
-		url: '',
+	title: '',
+	username: '',
+	url: '',
 });
 
 var Photocollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
-		sortProperties: ['title'],
-		sortAscending: true,
-		content: [],
+	sortProperties: ['title'],
+	sortAscending: true,
+	content: [],
 });
 
 var testPhotos = Photocollection.create();
 var testimg1 = Photo.create({
-		title: "Google logo",
-		username: "google",
-		url: "https://www.google.com/images/srpr/logo11w.png"
+	title: "Google logo",
+	username: "google",
+	url: "https://www.google.com/images/srpr/logo11w.png"
 });
 
 var testimg2 = Photo.create({
-		title: "UNO logo",
-		username: "UNO",
-		url: "http://www.unomaha.edu/_files/images/logo-subsite-o-2.png"
+	title: "UNO logo",
+	username: "UNO",
+	url: "http://www.unomaha.edu/_files/images/logo-subsite-o-2.png"
 });
 
 var testimg3 = Photo.create({
-		title: "Facebook Logo",
-		username: "Facebook",
-		url: "https://www.facebook.com/images/fb_icon_325x325.png"
+	title: "Facebook Logo",
+	username: "Facebook",
+	url: "https://www.facebook.com/images/fb_icon_325x325.png"
 });
+
+var testimg4 = Photo.create({
+		title: "Hubble Carina Nebula",
+		username: "NASA",
+		url: "http://imgsrc.hubblesite.org/hu/db/images/hs-2010-13-a-1920x1200_wallpaper.jpg"
+});		
+
 
 testPhotos.pushObject(testimg1);
 testPhotos.pushObject(testimg2);
 testPhotos.pushObject(testimg3);
+testPhotos.pushObject(testimg4);
 
 export default Ember.Controller.extend({
-		photos: testPhotos
+	photos: testPhotos,
+	searchField: '',
+	filteredPhotos: testPhotos,
+	actions: {
+			search: function () {
+					var filter = this.get('searchField');
+					var rx = new RegExp(filter, 'gi');
+					var photos = this.get('photos');
+					this.set('filteredPhotos',
+							photos.filter(function(photo){
+									return photo.get('title').match(rx) || photo.get('username').match(rx);
+							})
+					);
+			}
+	}
 });
